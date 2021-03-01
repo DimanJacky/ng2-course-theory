d8697da243343b91ca9aa15d5bdf912f74117af2
dmaslov <dmaslov@neoflex.ru>
2021-03-01 19:19:15 +0300

    commit

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,22 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-car',
-  template: `
-  <h2>Single Car</h2>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur doloremque facilis laudantium molestias mollitia numquam reiciendis ullam voluptatibus. A ab atque, debitis enim est fugit omnis quidem recusandae? Officia, sint?</p>
-  `,
+  templateUrl: './car.component.html',
   styles: [`
     h2 {
       color: aqua;
     }
   `]
 })
-export class CarComponent implements OnInit {
+export class CarComponent {
 
-  constructor() { }
+  carName = 'Ford';
+  carYear = 2015;
 
-  ngOnInit(): void {
+  getName() {
+    return this.carName;
   }
 
 }
